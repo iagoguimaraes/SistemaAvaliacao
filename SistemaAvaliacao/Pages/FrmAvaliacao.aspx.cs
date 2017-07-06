6fd4fd025008df8764003ea0926b0066cc8af35a
iago.guimaraes <iago.guimaraes@DES07-03CAL>
2017-07-06 18:08:58 -0300

    ajuste em popular e resgatar avaliacao

--- SistemaAvaliacao/Pages/FrmAvaliacao.aspx.cs
+++ SistemaAvaliacao/Pages/FrmAvaliacao.aspx.cs
@@ -3,6 +3,8 @@ using Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.HtmlControls;
@@ -16,18 +18,35 @@ namespace SistemaAvaliacao.Pages
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            Avaliacao avaliacao = ObterAvaliacao();
-            PopularAvaliacao(avaliacao);
+            try
+            {
+                Avaliacao avaliacao = ObterAvaliacao();
+                PopularAvaliacao(avaliacao);
+                Session["Avaliador"] = InicializarAvaliador(avaliacao);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
         protected void bt_finalizar_Click(object sender, EventArgs e)
         {
-
+            try
+            {
+                Avaliador avaliador = ResgatarAvaliador();
+                int x = 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
         #endregion
 
         #region Métodos
+
         private Avaliacao ObterAvaliacao()
         {
             using (AvaliacaoRepository repositoy = new AvaliacaoRepository())
@@ -43,30 +62,89 @@ namespace SistemaAvaliacao.Pages
             {
                 HtmlGenericControl html_questao = new HtmlGenericControl("div");
                 html_questao.Attributes["class"] = "questao";
-                html_questao.Attributes["id"] = questao.IdQuestao.ToString();
                 html_questao.InnerText = questao.Descricao;
+
+                RadioButtonList radio_alternativas = new RadioButtonList();
+                radio_alternativas.ID = "questao_" + questao.IdQuestao;
+                radio_alternativas.Attributes["class"] = "alternativas";
+                
 
                 foreach (Alternativa alternativa in questao.Alternativas)
                 {
-                    HtmlGenericControl html_alternativa = new HtmlGenericControl("div");
-                    html_alternativa.Attributes["class"] = "alternativa";
-
-                    HtmlGenericControl html_radio = new HtmlGenericControl("input");
-                    html_radio.Attributes["type"] = "radio";
-                    html_radio.Attributes["name"] = alternativa.IdQuestao.ToString();
-                    html_radio.Attributes["id"] = alternativa.IdAlternativa.ToString();
-
-                    HtmlGenericControl html_descricao = new HtmlGenericControl("label");
-                    html_descricao.InnerText = alternativa.Descricao;
-                    html_descricao.Attributes["for"] = alternativa.IdAlternativa.ToString();
-
-                    html_alternativa.Controls.Add(html_radio);
-                    html_alternativa.Controls.Add(html_descricao);
-                    html_questao.Controls.Add(html_alternativa);
+                    radio_alternativas.Items.Add(new ListItem(alternativa.Descricao, alternativa.IdAlternativa.ToString()));                   
                 }
 
+                html_questao.Controls.Add(radio_alternativas);
                 form_avaliacao.Controls.AddAt(form_avaliacao.Controls.Count-2, html_questao);
             }
+        }
+        private Avaliador InicializarAvaliador(Avaliacao avaliacao)
+        {
+            Avaliador avaliador = new Avaliador(avaliacao.IdAvaliacao);
+            avaliador.Dtini = DateTime.Now;
+            avaliador.Ip = string.Concat(GetIPAddress(), "|", GetLocalIPAddress());
+            foreach (var q in avaliacao.Questoes)
+            {
+                avaliador.Respostas.Add(new Resposta(q));
+            }
+            return avaliador;
+        }
+        private string GetIPAddress()
+        {
+            try
+            {
+                System.Web.HttpContext context = System.Web.HttpContext.Current;
+                string ipAddress = context.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+
+                if (!string.IsNullOrEmpty(ipAddress))
+                {
+                    string[] addresses = ipAddress.Split(',');
+                    if (addresses.Length != 0)
+                    {
+                        return addresses[0];
+                    }
+                }
+
+                return context.Request.ServerVariables["REMOTE_ADDR"];
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+        private string GetLocalIPAddress()
+        {
+            try
+            {
+                IPHostEntry host;
+                string localIP = "";
+                host = Dns.GetHostEntry(Dns.GetHostName());
+                foreach (IPAddress ip in host.AddressList)
+                {
+                    if (ip.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        localIP = ip.ToString();
+                        break;
+                    }
+                }
+                return localIP;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        private Avaliador ResgatarAvaliador()
+        {
+            Avaliador avaliador = (Avaliador)Session["Avaliador"];
+            avaliador.Dtfim = DateTime.Now;
+            foreach (var r in avaliador.Respostas)
+            {
+                RadioButtonList radio_alternativas = (RadioButtonList)form_avaliacao.FindControl("questao_" + r.IdQuestao);
+                r.IdAlternativa = Convert.ToInt32(radio_alternativas.SelectedValue);
+            }
+            return avaliador;
         }
 
         #endregion
